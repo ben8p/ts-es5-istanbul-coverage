669087c4dedb4e6f6b3b6428f8af3add1eca0e82
benjamin santalucia <ben@santalucia.me>
2018-05-28 09:17:18 +0200

    fix var coverage bug

--- src/ts-es5-istanbul-coverage.js
+++ src/ts-es5-istanbul-coverage.js
@@ -2,5 +2,6 @@ module.exports = function tsEs5IstanbulCoverage(content) {
 	return content
 		.replace(/(\|\| this;)/g, '/* istanbul ignore next */$1')
 		.replace(/(return _this;)([\n\r]+\s+)(var .*?;)/g, '$3$2$1')
+		.replace(/(return (_[a-zA-Z0-9]+))((?:.|[\n\r])*)(var \2;)/g, '$4$1$3')
 		.replace(/(for \(var _i = 0; _i < arguments\.length; _i\+\+\) \{)/g, '$1/* istanbul ignore next */');
 };
